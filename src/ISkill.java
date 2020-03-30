41a757d0807f4fe275c78ca62a7f334dc501e09e
Emerson Costa <syphonemerson@yahoo.com.br>
2020-03-30 16:10:03 -0300

    Apenas criado novo método da interface ISkill

--- src/ISkill.java
+++ src/ISkill.java
@@ -18,4 +18,20 @@ public interface ISkill
 		
 		return desviar;
 	}
+	
+	
+	// AINDA NÃO UTILIZADO, QUANDO FOR, PRECISA INFORMAR NO LOG QUE HOUVE AUMENTO DE VIDA
+	public static int AumentarVida() 
+	{
+		
+		int chance = rand.nextInt(3) + 1;
+		int valor = 0;
+		
+		if ( chance == 2 )
+		{
+			valor = 5;
+		}
+		
+		return valor;
+	}
 }
